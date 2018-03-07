cad0ed194e3a9ec51b9b67e6e3874eafb11bff96
Nathan Peart <RC0024792@student.riversidecollege.ac.uk>
2018-03-07 10:24:28 +0000

    Add project files.

---
+++ text.txt/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace text.txt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(@"H:\test.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    richTextBox1.AppendText(line + "\n");
+                }
+            }
+        }
+    }
+}
